b769a2029c0dc651def92dcd1c528193ed8d4904
Raouf-Medj <Raouf-Medj@github.com>
2025-04-27 20:39:16 +0200

    rextracted path2.ml  and graph2.ml

--- whyml/path2.ml
+++ whyml/path2.ml
@@ -1,18 +1,18 @@
 type path = (Z.t) list
 
-let dfs (g: ((Z.t) list) array) (v: Z.t) : (Z.t) list =
+let dfs (g: ((Z.t) list) array) (v': Z.t) : (Z.t) list =
   let visited = Array.make (Z.to_int (size g)) false in
   let accessible = ref ([] ) in
-  let rec branch (v1: Z.t) : unit =
-    if not visited.(Z.to_int v1)
+  let rec branch (v: Z.t) : unit =
+    if not visited.(Z.to_int v)
     then begin
-           visited.(Z.to_int v1) <- true;
-           accessible := v1 :: !accessible;
-           loop (succ g v1)
+           visited.(Z.to_int v) <- true;
+           accessible := v :: !accessible;
+           let succs = succ g v in loop succs
          end
   and loop (l: (Z.t) list) : unit =
     match l with
     | [] -> ()
     | neighbour :: s -> branch neighbour; loop s in
-  branch v; !accessible
+  branch v'; !accessible
 
